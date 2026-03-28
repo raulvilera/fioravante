26586d6c8ff94d24003db9f8ca6195907dc5ab27
raulvilera <124225576+raulvilera@users.noreply.github.com>
2026-03-28 09:46:37 -0300

    Update supabaseClient.ts

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://nemztmnfnkaitixyqskq.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_1r8jG8TECiSnS2csnX8_sg__LMuW1Lk';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5lbXp0bW5mbmthaXRpeHlxc2txIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNDkyODAsImV4cCI6MjA4OTgyNTI4MH0.pcC17gn-UIV3kWY9YMNG9e8PhMp9AQk-FfdKKYD_qzk';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
